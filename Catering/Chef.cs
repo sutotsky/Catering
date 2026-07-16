203fa67182661f18a164c8077573a6b2e5eae5c3
Christian Ugwoke <uchennachristian373@gmail.com>
2026-07-16 19:14:22 +0300

    Changes have been made and I also created class Chef

---
+++ Catering/Chef.cs
@@ -0,0 +1,6 @@
+﻿namespace Catering;
+
+public class Chef: Employee
+{
+    public string Cook { get; set; }
+}
--- Catering/Employee.cs
+++ Catering/Employee.cs
@@ -1,6 +1,17 @@
 ﻿namespace Catering;
 
-public class Employee: Address
+public class Employee
 {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime HireDateTime { get; set; }
+    
+    public string StreetName { get; set; }
+    public string CityName { get; set; }
+    public string StateName { get; set; }
+    public string ZipCode { get; set; }
     
 }
