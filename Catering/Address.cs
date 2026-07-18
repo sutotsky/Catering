6819fd1b49586d085756bc32d9489eef81537b69
Christian Ugwoke <uchennachristian373@gmail.com>
2026-07-18 09:18:04 +0300

    Revert "Employee data has been added"
    This reverts commit 06e85b94db25b2954f10aa26b65744a164d94832.

--- Catering/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Catering;
-
-public class Address: EmployeeData
-{
-    public string StreetName { get; set; }
-    public string CityName { get; set; }
-    public string StateName { get; set; }
-    public string ZipCode { get; set; }
-}
--- Catering/Employee.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Catering;
-
-public class Employee: Address
-{
-    
-}
